b31b9040cc8db97b59e0fe2ca2c612059e14b6cc
yoonwoo1000 <yoonwoo1000@gmail.com>
2021-07-08 10:11:24 +0900

    Add FeedUpdate.java

---
+++ app/src/main/java/com/example/protott/FeedUpdate.java
@@ -0,0 +1,14 @@
+package com.example.protott;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_update);
+    }
+}
--- app/src/main/java/com/example/protott/MainActivity.java
+++ app/src/main/java/com/example/protott/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.protott;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnFeedUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFeedUpdate = findViewById(R.id.btnFeedUpdate);
+
+        btnFeedUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FeedUpdate.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
